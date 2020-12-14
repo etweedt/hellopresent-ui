021ab59fef38e05db06ac4e69cd63ce002012df4
Eric Tweedt <etweedt@gmail.com>
2020-12-14 14:35:53 -0600

    Fixed notification formatting

--- src/components/notifications/notificationsUnseenContent.js
+++ src/components/notifications/notificationsUnseenContent.js
@@ -4,29 +4,31 @@ import {Alert} from 'reactstrap';
 
 function UnseenContent({notifications, onMarkSeen}) {
   return (
-    <>
-      {notifications.map(notification => {
-        return (
-          <div key={notification.id} className='row mt-3'>
+    <div className='row mt-3'>
+      <div className='col-sm'>
+        {notifications.map(notification => {
+          return (
+            <div key={notification.id} className='row'>
+              <div className='col-sm'>
+                <Alert color='info' toggle={() => onMarkSeen(notification)}>
+                  <div className='d-flex justify-content-between align-items-center'>
+                    <span>{notification.message}</span>
+                    <span>{new Date(notification.date).toLocaleString()}</span>
+                  </div>
+                </Alert>
+              </div>
+            </div>
+          );
+        })}
+        {notifications.length === 0 && (
+          <div className='row'>
             <div className='col-sm'>
-              <Alert color='info' toggle={() => onMarkSeen(notification)}>
-                <div className='d-flex justify-content-between align-items-center'>
-                  <span>{notification.message}</span>
-                  <span>{new Date(notification.date).toLocaleString()}</span>
-                </div>
-              </Alert>
+              <p>You have no new notifications!</p>
             </div>
           </div>
-        );
-      })}
-      {notifications.length === 0 && (
-        <div className='row mt-3'>
-          <div className='col-sm'>
-            <p>You have no new notifications!</p>
-          </div>
-        </div>
-      )}
-    </>
+        )}
+      </div>
+    </div>
   );
 }
 
